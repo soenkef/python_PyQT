d2bc46b0f5b38cfce546505f9dda7fccf4435aa1
soenkef <mantu@gmx.de>
2020-10-24 18:20:55 +0200

    PyQT - Tabelle gebaut

--- main.py
+++ main.py
@@ -12,34 +12,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.setWindowTitle("Studierendenverwaltung")
+        # self.setWindowTitle("BMI-Rechner")
 
-        self.ui.label_3.hide()
-        self.ui.result.hide()
+        self.ui.pushButton.clicked.connect(self.onPushButtonClick)
 
-        self.ui.calculate.clicked.connect(self.on_button_click)
+    def onPushButtonClick(self):
+        self.ui.tableWidget.setItem(0, 0, QtWidgets.QTableWidgetItem("Budapest"))
+        self.ui.tableWidget.setItem(0, 1, QtWidgets.QTableWidgetItem("9872345234"))
+        print("Button wurde geklickt!")
 
-    def on_button_click(self):
-        # self.ui.calculate.hide()
-
-        weight = self.ui.weight.value()
-        height = self.ui.height.value()
-
-        if height != 0 and weight != 0:
-            result = round(weight / (height ** 2), 2)
-
-            self.ui.label_3.show()
-            self.ui.label_3.setText("Dein BMI ist: ")
-            self.ui.result.show()
-            self.ui.result.setText(str(result))
-
-            print("on_button_click: " + str(result))
-        else:
-            self.ui.label_3.show()
-            self.ui.label_3.setText("Bitte gib gültige Werte ein!")
-            self.ui.weight.setValue(0)
-            self.ui.height.setValue(0.00)
-            self.ui.result.hide()
 window = MainWindow()
 
 window.show()
